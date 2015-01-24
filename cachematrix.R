26f377ceb540cf7922d2743c7fda9df2317e870c
Kirill Blinichkin <kirill.blinichkin@gmail.com>
2015-01-25 01:06:18 +0200

    Created a cached solve() function analog

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ------------------------------------------------------------
+## The script was created by Kirill Blinichkin at 24.01.2015. |
+##                                                            |
+## This script contains two helpful functions that allows     |
+## calculate inversion of matrices little faster than usual   |
+## solve() function.                                          |
+## ------------------------------------------------------------
 
-## Write a short comment describing this function
+
+## This function make from matrix a special object that 
+## can store inversion calculation.
+## x - a invertiyble matrix.
+## Return value is a special object.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInversion <- function(inversion) inv <<- inversion
+  
+  getInversion <- function() inv
+  
+  list(set = set, get = get,
+       setInversion = setInversion,
+       getInversion = getInversion)
 }
 
 
-## Write a short comment describing this function
+## This function do the same as solve() function, however, 
+## it caches the result of calculation.
+## x - a spatial object that is returned by makeCacheMatrix.
+## Return value is inversion of matrix that is contained in x.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInversion()
+  
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInversion(inv)
+  
+  inv
 }
